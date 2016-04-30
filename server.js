cccdace14c368e8066af19086659aa780187dfd7
Johannes Andersen <johanes1@online.no>
2016-04-30 18:56:04 +0200

    fnfr

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(webpackDevMiddleware(compiler, {
     }
 }));
 
-app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
